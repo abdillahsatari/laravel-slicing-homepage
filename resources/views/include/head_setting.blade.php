7d00157c535edbd0b8a7726bc75758ccfb23661c
Abdillah Satari Rahim <abdillah@fairtech.com.sg>
2020-03-15 17:29:02 +0800

    [Home Content] Applying Template in Project

---
+++ resources/views/include/head_setting.blade.php
@@ -0,0 +1,16 @@
+<!-- <link rel="manifest" href="site.webmanifest"> -->
+<link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/publics/img/favicon.png') }}">
+<!-- Place favicon.ico in the root directory -->
+
+<!-- CSS here -->
+<link rel="stylesheet" href="{{ asset('assets/publics/css/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/publics/css/owl.carousel.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/publics/css/magnific-popup.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/publics/css/font-awesome.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/publics/css/themify-icons.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/publics/css/nice-select.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/publics/css/flaticon.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/publics/css/gijgo.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/publics/css/animate.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/publics/css/slicknav.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/publics/css/style.css') }}">
